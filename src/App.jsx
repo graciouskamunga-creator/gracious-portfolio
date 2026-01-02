88ad94dd19406715212fddd9f08c28ccfcb61e61
Gracious Kamunga <graciouskamunga@gmail.com>
2026-01-02 05:39:41 -0800

    Update Navbar imports to lowercase

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Skills from "./pages/Skills";
 import Services from "./pages/Services";
+import Navbar from "./components/navbar";
 
-import Navbar from "../components/navbar";
 import ScrollToTop from "./components/ScrollToTop";
 import FloatingActionButton from "./components/FloatingActionButton";
 
