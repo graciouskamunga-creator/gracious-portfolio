3ffa939daf258dad644710aedfef0b1c28fd8f9d
Gracious Kamunga <graciouskamunga@gmail.com>
2026-01-07 06:12:24 -0800

    Fix duplicate header and layout structure

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { HelmetProvider } from 'react-helmet-async';
 import App from './App.jsx';
 import './index.css';
 import './styles.css';
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
